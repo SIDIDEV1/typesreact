ed0264de131be2c2a48fa7edee60b093902d2dca
SIDIDEV <sidimariko61@gmail.com>
2024-04-23 21:43:04 +0100

    forwardRef type

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,27 +1,23 @@
-import {
-  useRef,
-  useState,
-  type FunctionComponent,
-  type PropsWithChildren,
-} from "react";
+import { forwardRef, useRef, useState, type PropsWithChildren } from "react";
 
 type Props = PropsWithChildren<{
   start?: number;
 }>;
 
-export const Counter: FunctionComponent<Props> = ({ start = 0, children }) => {
-  const [n, setN] = useState<number>(start);
-  const ref = useRef<HTMLButtonElement>(null);
+export const Counter = forwardRef<HTMLButtonElement, Props>(
+  ({ start = 0, children }, ref) => {
+    const [n, setN] = useState<number>(start);
 
-  const incr = () => setN((n) => n + 1);
-  return (
-    <div>
-      <h1>Counter</h1>
-      <p>Number : {n}</p>
-      {children}
-      <button ref={ref} onClick={incr}>
-        Incremente
-      </button>
-    </div>
-  );
-};
+    const incr = () => setN((n) => n + 1);
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>Number : {n}</p>
+        {children}
+        <button ref={ref} onClick={incr}>
+          Incremente
+        </button>
+      </div>
+    );
+  }
+);
