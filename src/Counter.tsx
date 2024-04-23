657b8937b413d472526369543e49822d23bc1d85
SIDIDEV <sidimariko61@gmail.com>
2024-04-23 21:28:55 +0100

    Counter logique added

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,15 +1,22 @@
-import type { FunctionComponent, PropsWithChildren } from "react";
+import {
+  useState,
+  type FunctionComponent,
+  type PropsWithChildren,
+} from "react";
 
 type Props = PropsWithChildren<{
   start?: number;
 }>;
 
 export const Counter: FunctionComponent<Props> = ({ start = 0, children }) => {
+  const [n, setN] = useState<number>(start);
+
+  const incr = () => setN((n) => n + 1);
   return (
     <div>
       <h1>Counter</h1>
-      <p>Number : {start}</p>
-      <button>Incremente</button>
+      <p>Number : {n}</p>
+      <button onClick={incr}>Incremente</button>
     </div>
   );
 };
